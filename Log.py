68c9fc624e762677373bd6e097bd124e481c8221
ys <yunsong_nuaa@163.com>
2017-09-30 14:45:29 +0800

    +gridtest.py

--- Log.py
+++ Log.py
@@ -16,6 +16,14 @@ formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(level
 Rthandler.setFormatter(formatter)
 logging.getLogger('').addHandler(Rthandler)
 
+##################################################
+#定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+
 logger = logging.getLogger()
-
-

f2e5fdc26b234af06f3a2e4551a21aacb8a66587
yunsong86 <yunsong_nuaa@163.com>
2017-10-08 16:38:58 +0800

    w2v xgboost fasttext

--- FastText/train_fasttext.py
+++ FastText/train_fasttext.py
@@ -1,29 +1,31 @@
 # _*_coding:utf-8 _*_
 import logging
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 import fasttext
-#训练模型
-classifier = fasttext.supervised("D:/UbunutWin/corpus/fasttextcorpus/news_fasttext_train.txt","D:/UbunutWin/corpus/fasttextcorpus/news_fasttext.model",label_prefix="__label__")
 
-#load训练好的模型
-#classifier = fasttext.load_model('news_fasttext.model.bin', label_prefix='__label__')
-#测试模型
+# 训练模型
+classifier = fasttext.supervised("D:/UbunutWin/corpus/fasttextcorpus/news_fasttext_train.txt",
+                                 "D:/UbunutWin/corpus/fasttextcorpus/news_fasttext.model", label_prefix="__label__")
+
+# load训练好的模型
+# classifier = fasttext.load_model('news_fasttext.model.bin', label_prefix='__label__')
+# 测试模型
 result = classifier.test("D:/UbunutWin/corpus/fasttextcorpus/news_fasttext_test.txt")
-print( result.precision)
-print( result.recall)
-
+print(result.precision)
+print(result.recall)
 
 labels_right = []
 texts = []
 with open("D:/UbunutWin/corpus/fasttextcorpus/news_fasttext_test.txt", encoding='utf-8') as fr:
     lines = fr.readlines()
 for line in lines:
-    labels_right.append(line.split("\t")[1].rstrip().replace("__label__",""))
+    labels_right.append(line.split("\t")[1].rstrip().replace("__label__", ""))
     texts.append(line.split("\t")[0])
-#     print labels
+# print labels
 #     print texts
 #     break
-labels_predict = [e[0] for e in classifier.predict(texts)] #预测输出结果为二维形式
+labels_predict = [e[0] for e in classifier.predict(texts)]  # 预测输出结果为二维形式
 # print labels_predict
 
 text_labels = list(set(labels_right))
@@ -31,10 +33,10 @@ text_predict_labels = list(set(labels_predict))
 print(text_predict_labels)
 print(text_labels)
 
-A = dict.fromkeys(text_labels,0)  #预测正确的各个类的数目
-B = dict.fromkeys(text_labels,0)   #测试数据集中各个类的数目
-C = dict.fromkeys(text_predict_labels,0) #预测结果中各个类的数目
-for i in range(0,len(labels_right)):
+A = dict.fromkeys(text_labels, 0)  # 预测正确的各个类的数目
+B = dict.fromkeys(text_labels, 0)  # 测试数据集中各个类的数目
+C = dict.fromkeys(text_predict_labels, 0)  # 预测结果中各个类的数目
+for i in range(0, len(labels_right)):
     B[labels_right[i]] += 1
     C[labels_predict[i]] += 1
     if labels_right[i] == labels_predict[i]:
@@ -42,10 +44,10 @@ for i in range(0,len(labels_right)):
 
 print(A)
 print(B)
-print( C)
-#计算准确率，召回率，F值
+print(C)
+# 计算准确率，召回率，F值
 for key in B:
     p = float(A[key]) / float(B[key])
     r = float(A[key]) / float(C[key])
     f = p * r * 2 / (p + r)
-    print("%s:\tp:%f\t%fr:\t%f" % (key,p,r,f))
+    print("%s:\tp:%f\t%fr:\t%f" % (key, p, r, f))

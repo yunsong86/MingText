fa0c9539399780959deca2f1b6fada7b5fce3d64
yunsong86 <yunsong_nuaa@163.com>
2017-10-12 21:44:51 +0800

    w2v xgboost fasttext

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+"""
+@author: PANYUNSONG
+@file: config.py
+@time: 2017/10/11 22:36
+@desc: python3.6
+"""
+
+import platform
+if 'Windows' in platform.system():
+    DATA_PATH = "D:/UbuntuWin/"
+
+else:
+    DATA_PATH = "/mnt/hgfs/UbuntuWin/"
+
+
